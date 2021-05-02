b2677590213a383fac706d6f12db67d326855b89
abaur <abaur@student.42.fr>
2021-05-02 19:17:14 +0200

    List Constructors

--- list.hpp
+++ list.hpp
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/01 14:48:57 by abaur             #+#    #+#             */
-/*   Updated: 2021/05/01 16:00:31 by abaur            ###   ########.fr       */
+/*   Updated: 2021/05/02 19:16:47 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,6 @@ namespace ft
 	template <typename T, typename A = std::allocator<T> >
 	class list
 	{
-	// # PROTOTYPES
-	// ==================
 	public:
 		// ## Member types
 		typedef T	value_type;
@@ -41,6 +39,8 @@ namespace ft
 		typedef typename allocator_type::pointer        	pointer;
 		typedef typename allocator_type::const_pointer  	const_pointer;
 
+		friend	struct ft::list_iterator<      value_type,       list<T,A> >;
+		friend	struct ft::list_iterator<const value_type, const list<T,A> >;
 		typedef ft::list_iterator<      value_type,       list<T,A> >	iterator;
 		typedef ft::list_iterator<const value_type, const list<T,A> >	const_iterator;
 		typedef ft::reverse_iterator<iterator>      	reverse_iterator;
@@ -116,10 +116,126 @@ namespace ft
 		void	sort();
 		void	reverse();
 
-	// # IMPLEMENTATIONS
-	// ==================
+	private:
+		struct lselt {
+			value_type	value;
+			lselt*	prev;
+			lselt*	next;
 
+			lselt(const value_type& value, lselt* prev = NULL, lselt* next = NULL);
+			lselt(const lselt& other);
+			lselt&	operator=(const lselt& other);
+		};
+
+		allocator_type	_allocator;
+		size_type	_size;
+		lselt*	_first;
+		lselt*	_last;
 	};
+
+	// ## List Elements
+	template <typename T, typename A>
+	list<T,A>::lselt::lselt(const value_type& value, lselt* next, lselt* prev) {
+		this->value = value;
+		this->prev  = prev;
+		this->next  = next;
+	}
+	template <typename T, typename A>
+	list<T,A>::lselt::lselt(const lselt& other) {
+		this->value = other.value;
+		this->prev  = other.prev;
+		this->next  = other.next;
+	}
+	template <typename T, typename A>
+	typename list<T,A>::lselt&	list<T,A>::lselt::operator=(const lselt& other) {
+		this->value = other.value;
+		this->prev = other.prev;
+		this->next = other.next;
+	}
+
+
+	// ## Constructors
+	template<typename T, typename A>
+	list<T,A>::list(const allocator_type& allocator) {
+		this->_allocator = allocator;
+		this->_size  = 0;
+		this->_first = NULL;
+		this->_last  = NULL;
+	}
+
+	template <typename T, typename A>
+	list<T,A>::list(size_type size, const value_type& value, const allocator_type& allocator)
+	{
+		this->_allocator = allocator;
+		if (size <= 0) {
+			this->_size  = 0;
+			this->_first = NULL;
+			this->_last  = NULL;
+		}
+		else {
+			this->_first = new lselt(value);
+			this->_last  = _first;
+			for (this->_size=1; this->_size<size; this->_size++) {
+				lselt* prev = _last;
+				this->_last = new lselt(value, prev, NULL);
+				prev->next = _last;
+			}
+		}
+	}
+	template <typename T, typename A>
+	template <class IT>
+	list<T,A>::list(IT begin, IT end, const allocator_type& allocator) {
+		this->_allocator = allocator;
+		if (end <= begin) {
+			this->_size  = 0;
+			this->_first = NULL;
+			this->_last  = NULL;
+		}
+		else {
+			this->_first = new lselt(*begin);
+			this->_last  = _first;
+			this->_size  = 1;
+			for (IT it=begin+1; it<end; it++) {
+				lselt* prev = _last;
+				this->_last = new lselt(*it, prev, NULL);
+				prev->next  = _last;
+				_size++;
+			}
+		}
+	}
+	template <typename T, typename A>
+	list<T,A>::list(const list& other) {
+		this->_allocator = other._allocator;
+		if (other._size <= 0) {
+			this->_size  = 0;
+			this->_first = NULL;
+			this->_last  = NULL;
+		}
+		else {
+			this->_first = new lselt(other.front());
+			this->_last  =  _first;
+			this->_size  = 1;
+			for (lselt* ilt=other._first; ilt!=NULL; ilt=ilt->next) {
+				lselt* prev = this->_last;
+				this->_last = new lselt(ilt->value, prev, NULL);
+				prev->next = _last;
+				this->_size++;
+			}
+		}
+	}
+	template <typename T, typename A>
+	list<T,A>::~list() {
+		lselt* current = _first;
+		lselt* next;
+		while (current) {
+			next = current->next;
+			delete current;
+			current = next;
+		}
+	}
+
+	// list& operator=(const list& other);
+
 }
 
 #endif
