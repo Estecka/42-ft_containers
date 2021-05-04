57d4e151c8d35efe611c8b9c739d969c6be1915e
abaur <abaur@student.42.fr>
2021-05-04 19:01:55 +0200

    test.sh explicit leak verification

--- test.sh
+++ test.sh
@@ -10,7 +10,12 @@ make all || exit;
 for t in $TESTS;
 do;
 	echo "== $t ==";
+
 	./valgrind.sh $t.ft.leaks.log  $t.ft.out  > $t.ft.log;
 	./valgrind.sh $t.std.leaks.log $t.std.out > $t.std.log;
+
 	diff $t.std.log $t.ft.log && echo "OK";
+
+	grep <$t.ft.leaks.log "no leaks"         | sed -e 's/==[[:digit:]]*== //';
+	grep <$t.ft.leaks.log "LEAK SUMMARY" -A5 | sed -e 's/==[[:digit:]]*== //';
 done;
